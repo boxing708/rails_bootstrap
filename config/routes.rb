f77a1b669e978c6ed9279ae33738dd50f8100add
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-11 18:02:49 +0900

    add confirm back done routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :contacts
   resources :blogs
+  resources :contacts
+  post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
+  post 'contacts/back', to: 'contacts#back', as: 'back'
+  get 'done', to: 'contacts#done', as: 'done'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
