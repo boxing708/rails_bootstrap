8fa920fade0829d94fdd0082a646cec547312d1f
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-11 18:28:47 +0900

    fix contactmailer method_name

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -37,7 +37,7 @@ class ContactsController < ApplicationController
 
     respond_to do |format|
       if @contact.save
-        ContactMailer.send_mail(@contact).deliver_now
+        ContactMailer.contact_mail(@contact).deliver
         format.html { redirect_to contact_url(@contact), notice: "Contact was successfully created." }
         format.json { render :show, status: :created, location: @contact }
       else
