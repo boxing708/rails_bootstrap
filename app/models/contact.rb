be512d615ac7d10ccd7d53cf241ee5e560761c5a
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-11 18:29:50 +0900

    add validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :category, presence: true
+  validates :content, presence: true, length: { maximum: 140 }
 end
