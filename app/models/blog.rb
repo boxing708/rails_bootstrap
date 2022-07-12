1be15420785fdc264b0f3e4806f8310a3b4a9f93
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-12 09:45:03 +0900

    add validation to blog_model

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,4 @@
 class Blog < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 140 }
 end
