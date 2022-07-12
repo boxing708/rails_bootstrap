657959ae9d1b3c1fb76f3381d4dca9be1f1cba67
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-12 10:11:22 +0900

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root :to => "blogs#index"
   resources :blogs
   resources :contacts
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
