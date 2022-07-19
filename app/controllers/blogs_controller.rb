efad2f44950ed53c0eefc50d7aa5205fae97ecb8
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-19 14:57:55 +0900

    replace all to select and add includes(:user)

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.all
+    @blogs = Blog.select(:id, :title, :content, :user_id, :created_at).includes(:user)
   end
 
   # GET /blogs/1 or /blogs/1.json
