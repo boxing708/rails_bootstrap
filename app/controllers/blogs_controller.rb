0574ef3a8fc59a17e9789e93c464139511c3d1f2
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-19 15:22:09 +0900

    add scope :index_all

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs or /blogs.json
   def index
-    @blogs = Blog.select(:id, :title, :content, :user_id, :created_at).includes(:user)
+    @blogs = Blog.index_all.page(params[:page])
   end
 
   # GET /blogs/1 or /blogs/1.json
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,10 @@ class Blog < ApplicationRecord
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true, length: { maximum: 140 }
   belongs_to :user
+  paginates_per 10
+  scope :index_all, -> {
+  select(:id, :title, :content, :user_id, :created_at) #フィールドごとにレコードを取得
+  .order(created_at: :asc) #created_atカラムの昇順に並び替え
+  .includes(:user) #ユーザを事前に取得してN+1防止
+}
 end
