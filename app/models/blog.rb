52f175f4252cae4519dc90cfdf498d21a5fdb6a7
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-21 15:12:15 +0900

    add has_many favorite_users and add through and source to favorite_users

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -10,4 +10,5 @@ class Blog < ApplicationRecord
   }
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorite_users, through: :favorites, source: :user
 end
