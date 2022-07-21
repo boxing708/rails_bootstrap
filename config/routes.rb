c48755d3f851cacb23e611994d214a71d7afc45a
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-21 16:59:22 +0900

    change routes to add params[:blog_id]

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
   resources :contacts
-  resources :favorites, only: [:create, :destroy]
+  # resources :favorites, only: [:create, :destroy]
+  post 'favorite/:blog_id' => 'favorites#create', as: 'create_favorite'
+  delete 'favorite/:blog_id' => 'favorites#destroy', as: 'destroy_favorite'
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
   get 'done', to: 'contacts#done', as: 'done'
