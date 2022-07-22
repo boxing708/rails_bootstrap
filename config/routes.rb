968daa71662d8b77c00176ce066597311efa5f3b
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-22 17:13:40 +0900

    change favorites_routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,9 @@ Rails.application.routes.draw do
   resources :users, :only => [:index, :show]
   resources :blogs do
     resources :comments, only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
   end
   resources :contacts
-  # resources :favorites, only: [:create, :destroy]
-  post 'favorite/:blog_id' => 'favorites#create', as: 'create_favorite'
-  delete 'favorite/:blog_id' => 'favorites#destroy', as: 'destroy_favorite'
   post 'contacts/confirm', to: 'contacts#confirm', as: 'confirm'
   post 'contacts/back', to: 'contacts#back', as: 'back'
   get 'done', to: 'contacts#done', as: 'done'
