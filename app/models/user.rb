857ca609db61090a71423dd582039497e55dcc1d
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-22 12:32:21 +0900

    define association to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@ class User < ApplicationRecord
     .with_attached_icon #iconを事前に取得してN+1防止
   }
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 end
