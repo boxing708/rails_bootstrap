8f1c166ba89bdc4710e53b2861cd963a1a389c9c
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-22 15:21:24 +0900

    add favorite_blogs with through and source option

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,10 @@ class User < ApplicationRecord
     select(:id, :name, :email) #フィールドごとにレコードを取得
     .with_attached_icon #iconを事前に取得してN+1防止
   }
+  # Favorite
   has_many :favorites, dependent: :destroy
+  has_many :favorite_blogs, through: :favorites, source: :blog
+  # Relationship
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
 
