bb15dd49d8af09aa62fbd1223c23efcd76ed9239
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-25 14:36:27 +0900

    fix user_model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ApplicationRecord
   has_many :favorite_blogs, through: :favorites, source: :blog
   # Relationship
   has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
-  has_many :following, through: :active_relationships, source: :followed
+  has_many :followings, through: :active_relationships, source: :followed
 
   has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
