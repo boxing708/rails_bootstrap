7ebcbd9b0d0821e39f0c5bd991829cf475e53d9d
boxing708 <naoya.shishikura@super-studio.jp>
2022-07-25 14:29:06 +0900

    create create/destroy action

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,16 @@
 class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    following = current_user.active_relationships.build(followed_id: params[:user_id])
+    following.save
+    # request.referrerと書くことで、以前のpathに戻ることができる
+    redirect_to request.referrer || root_path
+  end
+
+  def destroy
+    following = current_user.active_relationships.find_by(followed_id: params[:user_id])
+    following.destroy
+    # request.referrerと書くことで、以前のpathに戻ることができる
+    redirect_to request.referrer || root_path
+  end
 end
